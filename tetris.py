f169c08930e94acf7fe364259d003a391efc14f1
shubham1208 <shubhamsahutheperfect@gmail.com>
2020-12-23 15:07:18 +0530

    final touches

--- tetris.py
+++ tetris.py
@@ -146,14 +146,14 @@ class Piece(object):
         self.rotation = 0  # number from 0-3
 
 
-def create_grid(locked_positions={}):
-    grid = [[(0,0,0) for x in range(10)] for x in range(20)]
-
-    for i in range(len(grid)):
+def create_grid(locked_pos={}):
+    grid = [[(0,0,0)for _ in range (10)] for _ in range (20)]
+    for i in range (len(grid)):
         for j in range(len(grid[i])):
-            if (j,i) in locked_positions:
-                c = locked_positions[(j,i)]
+            if (j, i) in locked_pos:
+                c = locked_pos[(j, i)]
                 grid[i][j] = c
+
     return grid
 
 
