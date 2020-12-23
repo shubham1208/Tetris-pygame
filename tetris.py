8465aafd751969b0d08e7a8c4fc6731209b3ee33
shubham1208 <shubhamsahutheperfect@gmail.com>
2020-12-23 11:21:38 +0530

    Update tetris.py and add grid

--- tetris.py
+++ tetris.py
@@ -158,10 +158,29 @@ def create_grid(locked_pos={}):
 
 
 def convert_shape_format(shape):
-    pass
+    positions = []
+    format = shape.shape(shape.rotation % len(shape.shape))
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x+j, shape.y+i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
 
 def valid_space(shape, grid):
-    pass
+    accepted_pos = [[(j, i) for j in range(10)] for i in range(20)]
+    accepted_pos = [j for sub in accepted_pos for j in sub] 
+    formatted = convert_shape_format(shape)
+
+    for pos in formatted:
+        if pos not in accepted_pos:
+            if pos[1] > -1:
+                return False
+    return True
+
 
 def check_lost(positions):
     pass
